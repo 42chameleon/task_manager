fab128c40fffd636955047b1c8e174da70a7e2fa
Muhammad Israpilov <42israpilov@mail.ru>
2021-04-15 23:17:23 +0300

    added model customuser

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password, **extra_fields):
+
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+        return self.create_user(username, password, **extra_fields)
+
+
+class CustomUser(AbstractUser):
+    USERNAME_FIELD = 'username'
+    full_name = models.CharField(max_length=1024)
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
